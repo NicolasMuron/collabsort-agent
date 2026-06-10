5301affe21eeb92d14bbd107219ab0df8523681b
NicolasMuron <nicolas.muron@gmail.com>
2026-06-10 11:20:56 +0200

    Test avec ARD au lieu de eps

--- src/collabsort_agent/decision/decision.py
+++ src/collabsort_agent/decision/decision.py
@@ -17,7 +17,7 @@ class Config:
     """Decision configuration"""
 
     # Deision algorithm to use
-    algorithm: Literal["eps", "ard"] = "eps"
+    algorithm: Literal["eps", "ard"] = "ard"
 
     # ---------- Exploration decay ----------
 
