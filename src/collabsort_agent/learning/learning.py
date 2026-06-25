11468ece4c1b2623e08e9bb9a45bf13eca39a542
NicolasMuron <nicolas.muron@gmail.com>
2026-06-25 15:31:22 +0200

    Retour au dqn basique pour tester les récompenses stochastiques

--- src/collabsort_agent/learning/learning.py
+++ src/collabsort_agent/learning/learning.py
@@ -26,7 +26,7 @@ class Config:
         "n_step",
         "noisy",
         "rainbow",
-    ] = "noisy"
+    ] = "dqn"
 
     # Discount factor for Temporal-Difference algorithms
     gamma: float = 0.99
