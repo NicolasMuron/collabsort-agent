0ff63a7d424d0250bcb31a30288834585e922574
NicolasMuron <nicolas.muron@gmail.com>
2026-06-30 14:11:16 +0200

    Test avec 7 colonnes percues

--- src/collabsort_agent/perception/perception.py
+++ src/collabsort_agent/perception/perception.py
@@ -12,7 +12,7 @@ class Config:
     """Perception configuration"""
 
     # Number of perceived columns in an observation
-    n_perceived_cols: int = 6
+    n_perceived_cols: int = 7
 
 
 class Perceiver:
