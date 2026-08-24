249b786c4218f6e0b7726a252d226ddfdbb151cf
NicolasMuron <nicolas.muron@gmail.com>
2026-08-24 13:56:33 +0200

    Test en baissant la proba jusqu'à 0 sur une décroissance exponentielle

--- src/collabsort_agent/decision/decision.py
+++ src/collabsort_agent/decision/decision.py
@@ -28,7 +28,7 @@ class DecisionConfig:
     epsilon_min: float = 0
 
     # Exploration probability decay algorithm
-    exploration_decay: Literal["lin", "exp"] = "lin"
+    exploration_decay: Literal["lin", "exp"] = "exp"
 
     # Percentage of training time during which exploration probability is decayed
     decay_span: float = 0.5
