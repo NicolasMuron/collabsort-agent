9f91743734e970c185000bde456d82c32d28fb6b
NicolasMuron <nicolas.muron@gmail.com>
2026-08-24 15:23:06 +0200

    Correction d'une erreur liée à la décroissance exp et test PER rec opti

--- src/collabsort_agent/decision/decision.py
+++ src/collabsort_agent/decision/decision.py
@@ -28,10 +28,10 @@ class DecisionConfig:
     epsilon_min: float = 0
 
     # Exploration probability decay algorithm
-    exploration_decay: Literal["lin", "exp"] = "lin"
+    exploration_decay: Literal["lin", "exp"] = "exp"
 
     # Percentage of training time during which exploration probability is decayed
-    decay_span: float = 0.75
+    decay_span: float = 0.5
 
     # If enabled, reset the exploration decay at the start of each curriculum phase.
     reset_exploration_per_phase: bool = False
