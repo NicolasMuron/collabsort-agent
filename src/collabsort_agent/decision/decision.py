2c70d85afcbaf16bc6ce332f2455223c3de03260
NicolasMuron <nicolas.muron@gmail.com>
2026-08-26 13:35:10 +0200

    Test DQN RL classique avec explo

--- src/collabsort_agent/decision/decision.py
+++ src/collabsort_agent/decision/decision.py
@@ -22,7 +22,7 @@ class DecisionConfig:
     # ---------- Exploration decay ----------
 
     # Starting exploration probability
-    epsilon_start: float = 0
+    epsilon_start: float = 1.0
 
     # Minimum exploration probability at the end of decay
     epsilon_min: float = 0
